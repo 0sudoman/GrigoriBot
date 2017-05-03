b72ed0c9305c93bf118acb5d27e6dff410781f07
Justin Dickerson <0sudoman@gmail.com>
2017-05-03 01:34:11 -0400

    general fixes, moved some stuff

--- bot.sh
+++ bot.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 # bot.sh
 # responds to commands
+# this is seperate now so it can be edited on-the-fly without rebooting
 
 source config.sh
 SCRIPT="bot.sh"
-sendToLog "Started"
 
-while true; do
-# grab a new line
-NEW="$( tail -n1 $IRC/out )"
-if [ "$LINE" != "$NEW" ]; then LINE="$NEW"
+LINE="$1"
 if [[ "$LINE" =~ ^.{17}\<(.*)\>\ (.*)$ ]]; then SENDER="${BASH_REMATCH[1]}"; MESSAGE="${BASH_REMATCH[2]}"; fi
+if [[ "$MESSAGE" =~ ^(.*)\ (.*)$ ]]; then MESSAGE="${BASH_REMATCH[1]},,"; EXTRA="${BASH_REMATCH[2]}"
+else MESSAGE="${MESSAGE,,}"; fi
+if [[ $SENDER != $BOTNICK ]]; then
 
 # public commands
 if [[ "$MESSAGE" == "!help" ]]; then sendToIRC "You can find my docs at https://github.com/0sudoman/GrigoriBot/blob/master/docs.md"; fi
-
 if [[ "$MESSAGE" == "!source" ]]; then sendToIRC "https://github.com/0sudoman/GrigoriBot"; fi
 if [[ "$MESSAGE" == "${BOTNICK}++" ]]; then sendToIRC ":D"; fi
 if [[ "$MESSAGE" == "${BOTNICK}--" ]]; then sendToIRC "D:"; fi
 
 # admin commands
 if [[ $SENDER == $ADMIN ]]; then
-if [[ "$MESSAGE" =~ "!sort "(.*) ]]; then ./sort.sh "${BASH_REMATCH[1]}"; fi
-#if [[ "$MESSAGE" == "!reboot" ]]; then sendToIRC "Rebooting all systems."; ./reboot.sh; fi
-if [[ "$MESSAGE" == "!shutdown" ]]; then sendToIRC "Shutting down all systems."; ./shutdown.sh; fi
+if [[ "$MESSAGE" =~ "!sort" ]]; then ./sort.sh "$EXTRA"; fi
+#if [[ "$MESSAGE" == "!reboot" ]]; then sendToIRC "Rebooting all systems."; botReboot; fi
+#if [[ "$MESSAGE" == "!shutdown" ]]; then sendToIRC "Shutting down all systems."; botShutdown; fi
 fi
 
 fi
-done
